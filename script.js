d52575710ce5b0e8d6f314fd95668d551a6a7b43
Rohit Prajapati <65508466+rohu0510@users.noreply.github.com>
2023-01-12 00:23:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 function threeSum(arr, target) {
   //your code here
+	arr.sort((a, b) => a-b);
+
+	let csum= infinity;
+	for(let i=0; i<arr.length-2; i++){
+		let l=i+1, r=arr.length-1;
+		while (l <r) {
+			let currsum = arr[i]+arr[l]+arr[r];
+			if(Math.abs(currsum - target) < Math.abs(csum-target)){
+				csum = currsum;
+			}
+			if(currsum < target){
+				l++;
+			}
+			else{
+				r++;
+			}
+		}
+	}
+	return csum;
 }
 
 module.exports = threeSum;
